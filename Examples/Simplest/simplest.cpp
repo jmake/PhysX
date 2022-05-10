0a61bf50406e57d9b27456e6145a6052aabd1feb
jmake <zvl_2006@hotmail.com>
2022-05-10 21:25:06 +0200

    Update simplest.cpp

--- Examples/Simplest/simplest.cpp
+++ Examples/Simplest/simplest.cpp
@@ -1,17 +1,16 @@
+include <iostream>
 
-#include <iostream>
+#include <PxConfig.h>
+#include <PxPhysicsAPI.h>
 
-#include <PxConfig.h> 
-#include <PxPhysicsAPI.h> 
-
-#include <extensions\PxExtensionsAPI.h>
-#include <extensions\PxDefaultErrorCallback.h>
-#include <extensions\PxDefaultAllocator.h> 
-#include <extensions\PxDefaultSimulationFilterShader.h>
-#include <extensions\PxDefaultCpuDispatcher.h>
-#include <extensions\PxShapeExt.h>
-#include <foundation\PxMat33.h> 
-#include <extensions\PxSimpleFactory.h>
+#include <extensions/PxExtensionsAPI.h>
+#include <extensions/PxDefaultErrorCallback.h>
+#include <extensions/PxDefaultAllocator.h>
+#include <extensions/PxDefaultSimulationFilterShader.h>
+#include <extensions/PxDefaultCpuDispatcher.h>
+#include <extensions/PxShapeExt.h>
+#include <foundation/PxMat33.h>
+#include <extensions/PxSimpleFactory.h>
 
 static physx::PxFoundation* gFoundation = NULL;
 static physx::PxPhysics* gPhysicsSDK = NULL;
@@ -20,32 +19,36 @@ static physx::PxDefaultErrorCallback gDefaultErrorCallback;
 static physx::PxDefaultAllocator gDefaultAllocatorCallback;
 
 
-void InitializePhysX() 
+void InitializePhysX()
 {
-	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
-	gPhysicsSDK = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale());
+  gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
+  gPhysicsSDK = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, physx::PxTolerancesScale());
 
-	if(gPhysicsSDK==NULL) 
+  if(gPhysicsSDK==NULL)
   {
-		cerr<<"Error creating PhysX device."<<endl;
-		cerr<<"Exiting..."<<endl;
-		exit(1);
-	}
-  
-  std::cout<<"[InitializePhysX]"<<std::endl; 
+    std::cerr<<"Error creating PhysX device."<<std::endl;
+    std::cerr<<"Exiting..."<<std::endl;
+    std::exit(1);
+  }
+
+  std::cout<<"[InitializePhysX]"<<std::endl;
 }
 
-void ShutdownPhysX() 
+
+void ShutdownPhysX()
 {
-	gPhysicsSDK->release();
-  std::cout<<"[ShutdownPhysX]"<<std::endl; 
+  gPhysicsSDK->release();
+  std::cout<<"[ShutdownPhysX]"<<std::endl;
 }
 
 
-int 
+int
 main(int argc, char** argv)
 {
-  InitializePhysX(); 
-  ShutdownPhysX(); 
-  return 0; 
+  InitializePhysX();
+  ShutdownPhysX();
+  return 0;
 }
+
+
+
