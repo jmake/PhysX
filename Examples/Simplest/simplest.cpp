3572e6f963253a39bf7f9072284fe5b87f27eb56
jmake <zvl_2006@hotmail.com>
2022-05-10 19:08:05 +0200

    Create simplest.cpp

---
+++ Examples/Simplest/simplest.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+#include <PxConfig.h> 
+#include <PxPhysicsAPI.h> 
+
+#include <extensions\PxExtensionsAPI.h>
+#include <extensions\PxDefaultErrorCallback.h>
+#include <extensions\PxDefaultAllocator.h> 
+#include <extensions\PxDefaultSimulationFilterShader.h>
+#include <extensions\PxDefaultCpuDispatcher.h>
+#include <extensions\PxShapeExt.h>
+#include <foundation\PxMat33.h> 
+#include <extensions\PxSimpleFactory.h>
+
+static physx::PxFoundation* gFoundation = NULL;
+static physx::PxPhysics* gPhysicsSDK = NULL;
+
+static physx::PxDefaultErrorCallback gDefaultErrorCallback;
+static physx::PxDefaultAllocator gDefaultAllocatorCallback;
+
+
+void InitializePhysX() 
+{
+	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
+	gPhysicsSDK = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale());
+
+	if(gPhysicsSDK==NULL) 
+  {
+		cerr<<"Error creating PhysX device."<<endl;
+		cerr<<"Exiting..."<<endl;
+		exit(1);
+	}
+  
+  std::cout<<"[InitializePhysX]"<<std::endl; 
+}
+
+void ShutdownPhysX() 
+{
+	gPhysicsSDK->release();
+  std::cout<<"[ShutdownPhysX]"<<std::endl; 
+}
+
+
+int 
+main(int argc, char** argv)
+{
+  InitializePhysX(); 
+  ShutdownPhysX(); 
+  return 0; 
+}
