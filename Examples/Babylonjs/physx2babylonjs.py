ee287f26310af044eb4143398ddf3f4a8804c57e
jmake <zvl_2006@hotmail.com>
2022-05-15 17:37:47 +0200

    Update physx2babylonjs.py

--- Examples/Babylonjs/physx2babylonjs.py
+++ Examples/Babylonjs/physx2babylonjs.py
@@ -1,6 +1,6 @@
 
 template = open("/home/Examples/Babylonjs/babylon_template.html","r").read()
-fieldsjs = open("/home/fields_new.js","r").read()
+fieldsjs = open("/home/Examples/Babylonjs/BUILD/fields_new.js","r").read()
 
 f = open("babylon.html","w")
 f.write(template.replace("fieldsJS",fieldsjs))
