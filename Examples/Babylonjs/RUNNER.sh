6c896d5240c554a7df679c48aedef5460e69c1a9
jmake <zvl_2006@hotmail.com>
2022-05-15 17:17:59 +0200

    Create RUNNER.sh

---
+++ Examples/Babylonjs/RUNNER.sh
@@ -0,0 +1,35 @@
+CHECKING()
+{
+  echo "[DOCKER] LIBRARIES"
+  ls -la /DUMMY/physx/install/linux/PhysX/bin/linux.clang/release/
+  
+  echo "[DOCKER] INCLUDES:"
+  ls /DUMMY/physx/install/linux/PhysX/include/PxPhysicsAPI.h
+  ls /DUMMY/physx/install/linux/PxShared/include/foundation/Px.h
+}
+
+
+SIMULATION_RUN() 
+{
+  cd /home/Examples/Simplest
+
+  mkdir BUILD
+  cd BUILD
+
+  cmake .. -DCMAKE_RULE_MESSAGES=OFF
+  
+  make
+
+  ctest
+}
+
+echo "[DOCKER] ls"
+ls
+
+echo "[DOCKER] pwd"
+pwd
+
+echo "[DOCKER] ..."
+
+CHECKING
+SIMULATION_RUN
