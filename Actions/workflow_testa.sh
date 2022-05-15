65febedf19a69dd22015ef893e34179fcac7c612
jmake <zvl_2006@hotmail.com>
2022-05-15 17:22:29 +0200

    Update workflow_testa.sh

--- Actions/workflow_testa.sh
+++ Actions/workflow_testa.sh
@@ -40,6 +40,10 @@ DOCKER_RUN
 TOBEEXECUTED="bash Examples/Simplest/RUNNER.sh"
 DOCKER_RUN
 
+TOBEEXECUTED="bash Examples/Babylonjs/RUNNER.sh"
+DOCKER_RUN
+
+
 #
 # docker run --rm --name nvcc_container --volume ${PWD}:/home --workdir /home -i nvcc_image bash GET_NVCC.sh
 # 
